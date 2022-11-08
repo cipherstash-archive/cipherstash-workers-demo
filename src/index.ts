5b9b9cf894d6d40a7543419569418ea6becf8155
Bennett Hardwick <bennett@cipherstash.com>
2022-11-08 14:00:37 +1000

    Add secure endpoint

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,11 @@ import { Stash, debug } from "@cipherstash/stashjs-worker";
 import { HandlerError, isValidUuid } from "./utils";
 import { serveStaticFile } from "./static";
 
+import { v4 as uuidv4 } from "uuid";
+
 import SCHEMA from "../users.annotated.json";
+
+import z from "zod";
 
 export interface Env {
   // The host of the CipherStash instance
@@ -14,6 +18,30 @@ export interface Env {
   // KVNamespace used by Worker Sites to serve the static app part of the worker
   __STATIC_CONTENT: KVNamespace;
 }
+
+function dateString() {
+  return z.preprocess((x): Date | undefined => {
+    if (typeof x === "string") {
+      const date = new Date(x);
+
+      if (!isNaN(Number(date))) {
+        return date;
+      }
+    }
+
+    return undefined;
+  }, z.date());
+}
+
+const PatientRecord = z.object({
+  name: z.string(),
+  dob: dateString(),
+  phone: z.string(),
+  gender: z.string(),
+  socialSecurityNumber: z.string(),
+  medicalConditions: z.string(),
+  comments: z.string(),
+});
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
@@ -45,7 +73,28 @@ export default {
       router.post(
         "/secure",
         withStash(async (request, stash) => {
-          throw new HandlerError("Unimplemented!", 500);
+          const result = PatientRecord.safeParse(await request.json());
+
+          if (!result.success) {
+            throw new HandlerError(
+              "Failed to parse record from request body",
+              400
+            );
+          }
+
+          const record = result.data;
+
+          const id = uuidv4();
+
+          await stash.put({
+            ...record,
+            id,
+          });
+
+          return Response.json({
+            success: true,
+            id,
+          });
         })
       );
 
