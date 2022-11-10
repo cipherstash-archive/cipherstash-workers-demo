e360db09bc74e0344dfefa5f657f199fd963574d
Bennett Hardwick <bennett@cipherstash.com>
2022-11-10 16:15:35 +1000

    Add set source key script

--- deploy-pages.sh
+++ deploy-pages.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-export VITE_API_DOMAIN=$1
+set -euo pipefail
+
+export VITE_API_DOMAIN="${1:-}"
+
+if [ -z "${VITE_API_DOMAIN}" ]; then
+  echo "Error: expected workers domain to be passed through."
+  echo ""
+  echo "Usage: npm run publish:app <workers domain>"
+  exit 1
+fi
 
 npm run app:build
 
