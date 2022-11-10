a0d2bad67c79c1792a5ae00959c3135009225f87
Bennett Hardwick <bennett@cipherstash.com>
2022-11-10 16:15:35 +1000

    Add set source key script

---
+++ set-source-key.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "Generating a 16 byte encryption key."
+echo ""
+echo "Setting CIPHERSTASH_KEY secret using wrangler."
+
+node -e "let { randomBytes } = require('crypto'); console.log(randomBytes(16).toString('hex'))" | wrangler secret put CIPHERSTASH_KEY
