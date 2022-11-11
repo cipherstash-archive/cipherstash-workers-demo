e218931fa43084c53a883eb97258b4b271a76fbb
Lindsay Holmwood <lindsay@holmwood.id.au>
2022-11-11 16:12:48 +1100

    Capitalise correctly

--- set-source-key.sh
+++ set-source-key.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 echo "Generating a 16 byte encryption key."
 echo ""
-echo "Setting CIPHERSTASH_KEY secret using wrangler."
+echo "Setting CIPHERSTASH_KEY secret using Wrangler."
 
 node -e "let { randomBytes } = require('crypto'); console.log(randomBytes(16).toString('hex'))" | wrangler secret put CIPHERSTASH_KEY
