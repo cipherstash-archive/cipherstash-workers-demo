baa3c1a3420edbba91fe1809a92a37ebcce147db
Bennett Hardwick <bennett@cipherstash.com>
2022-11-14 16:36:10 +1000

    Move around, add crypto, comments

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { Router } from "itty-router";
-import { Stash } from "@cipherstash/stashjs-worker";
 
 import { HandlerError } from "./utils";
 import { PatientRecord, PatientRecordQuery, decodeQuery } from "./patient";
 
 import { v4 as uuidv4 } from "uuid";
 
-import SCHEMA from "../patients.annotated.json";
+import { withCors } from "./withCors";
+import { deleteCookie, withStash } from "./withStash";
+import { withAdminAuth } from "./withAdminAuth";
 
 export interface Env {
   CIPHERSTASH_CLIENT_SECRET: string;
@@ -16,144 +17,13 @@ export interface Env {
   ADMIN_AUTH_PASSWORD: string;
 }
 
-const TOKEN_COOKIE_NAME = "cs_access_token";
-
-// Cloudflare workers supports toGMTString
-declare global {
-  interface Date {
-    toGMTString(): string;
-  }
-}
-
-function setCookie(req: Request, res: Response, token: string, expires?: Date) {
-  res.headers.append(
-    "set-cookie",
-    // todo: see whether this could be UTC instead of GMT
-    `${TOKEN_COOKIE_NAME}=${token}; SameSite=None; Secure; expires=${(
-      expires ?? new Date(0)
-    ).toGMTString()}`
-  );
-}
-
-function withAuth(
-  env: Env,
-  handler: (request: Request, token: string) => Promise<Response>
-) {
-  return async (request: Request): Promise<Response> => {
-    const cookies = `; ${request.headers.get("cookie") ?? ""}`;
-    const parts = cookies.split(`; ${TOKEN_COOKIE_NAME}=`);
-
-    if (parts.length === 2) {
-      const token = parts.pop()?.split(";").shift();
-
-      if (token) {
-        return await handler(request, token);
-      }
-    }
-
-    const response = await fetch(
-      "https://console.cipherstash.com/api/authorise",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          accessKey: env.CIPHERSTASH_CLIENT_SECRET.trim(),
-        }),
-        headers: {
-          "content-type": "application/json",
-        },
-      }
-    );
-
-    if (response.status !== 200) {
-      throw new HandlerError(
-        "Failed to exchange auth token",
-        401,
-        await response.json<{}>().catch(() => undefined)
-      );
-    }
-
-    const { accessToken, expiry } = await response.json<{
-      accessToken: string;
-      expiry: number;
-    }>();
-
-    const expires = new Date(expiry * 1000 - 10 * 1000);
-
-    const res = await handler(request, accessToken);
-
-    setCookie(request, res, accessToken, expires);
-
-    return res;
-  };
-}
-
-function withCors(handler: (request: Request, env: Env) => Promise<Response>) {
-  return async (request: Request, env: Env): Promise<Response> => {
-    const response =
-      request.method === "OPTIONS"
-        ? new Response(null)
-        : await handler(request, env);
-
-    const origin = request.headers.get("origin");
-
-    if (!origin) {
-      return response;
-    }
-
-    response.headers.append("Access-Control-Allow-Origin", origin);
-    response.headers.append("Access-Control-Allow-Methods", "POST");
-    response.headers.append(
-      "Access-Control-Allow-Headers",
-      "content-type,authorization"
-    );
-    response.headers.append("Access-Control-Allow-Credentials", "true");
-    response.headers.append("Access-Control-Max-Age", "600");
-
-    return response;
-  };
-}
-
 export default {
   fetch: withCors(async (request, env) => {
     try {
       const router = Router();
-
-      function withStash(
-        handler: (request: Request, stash: Stash) => Promise<Response>
-      ) {
-        return withAuth(env, async (request, token) => {
-          const stash = Stash.fromAnnotatedSchema(SCHEMA, {
-            host: `https://${SCHEMA.service.host}`,
-            key: env.CIPHERSTASH_KEY,
-            token,
-          });
-
-          return await handler(request, stash);
-        });
-      }
-
-      function withAdminAuth(handler: (request: Request) => Promise<Response>) {
-        return async (request: Request): Promise<Response> => {
-          const auth = request.headers.get("authorization");
-
-          if (!auth) {
-            throw new HandlerError("Expected auth header", 401);
-          }
-
-          const [, encoded] = auth.split("Basic ");
-          const decoded = atob(encoded);
-
-          if (decoded !== `admin:${env.ADMIN_AUTH_PASSWORD}`) {
-            throw new HandlerError("Invalid credentials", 401);
-          }
-
-          return await handler(request);
-        };
-      }
-
       router.post(
         "/secure",
-        withStash(async (request, stash) => {
+        withStash(env, async (request, stash) => {
           const result = PatientRecord.safeParse(await request.json());
 
           if (!result.success) {
@@ -180,10 +50,13 @@ export default {
         })
       );
 
+      // The search endpoint is used to query patient information.
+      //
       router.post(
         "/search",
         withAdminAuth(
-          withStash(async (request, stash) => {
+          env,
+          withStash(env, async (request, stash) => {
             const result = PatientRecordQuery.safeParse(await request.json());
 
             if (!result.success) {
@@ -215,30 +88,35 @@ export default {
         )
       );
 
+      // This endpoint is used to check whether the Basic authentication provided by the user is correct
+      // Validation is handled by the withAdminAuth function
       router.post(
         "/login",
-        withAdminAuth(async (request) => Response.json({ success: true }))
+        withAdminAuth(env, async () => Response.json({ success: true }))
       );
 
+      // If the worker doesn't have
       router.all("*", () => new Response(null, { status: 404 }));
 
       return await router.handle(request);
     } catch (e) {
-      console.error("An error occurred:", e);
+      let response: Response;
 
-      const response =
-        e instanceof HandlerError
-          ? e.toResponse()
-          : Response.json(
-              {
-                success: false,
-                error: String(e),
-              },
-              { status: 500 }
-            );
+      if (e instanceof HandlerError) {
+        response = e.toResponse();
+      } else {
+        console.error("An unexpected error occurred", e);
+        response = Response.json(
+          {
+            success: false,
+            error: String(e),
+          },
+          { status: 500 }
+        );
+      }
 
       // delete cookie if there is an error response
-      setCookie(request, response, "");
+      deleteCookie(response);
 
       return response;
     }
---
+++ src/withStash.ts
@@ -0,0 +1,166 @@
+import type { Env } from ".";
+import { HandlerError } from "./utils";
+import SCHEMA from "../patients.annotated.json";
+import { Stash } from "@cipherstash/stashjs-worker";
+
+const TOKEN_COOKIE_NAME = "cs_access_token";
+
+// Cloudflare workers supports toGMTString
+declare global {
+  interface Date {
+    toGMTString(): string;
+  }
+}
+
+function setCookie(res: Response, token: string, expires: Date) {
+  res.headers.append(
+    "set-cookie",
+    // todo: see whether this could be UTC instead of GMT
+    `${TOKEN_COOKIE_NAME}=${token}; SameSite=None; Secure; expires=${expires.toGMTString()}`
+  );
+}
+
+/**
+ * Set an expired access token cookie so that it gets deleted
+ */
+export function deleteCookie(res: Response) {
+  setCookie(res, "", new Date(new Date().getTime() + 1000 * 60 * 60));
+}
+
+function bufferFromHex(hex: string): Uint8Array {
+  return new Uint8Array(
+    hex.match(/[\da-f]{2}/gi)?.map((h) => parseInt(h, 16)) ?? []
+  );
+}
+
+function bufferToHex(buffer: Uint8Array): string {
+  return Array.from(buffer)
+    .map((x) => x.toString(16).padStart(2, "0"))
+    .join("");
+}
+
+function withAuth(
+  env: Env,
+  handler: (request: Request, token: string) => Promise<Response>
+) {
+  return async (request: Request): Promise<Response> => {
+    // Instead of storing user sessions in a durable object we store a CipherStash access token encrypted as a cookie.
+    // When users make requests to the service this method checks for and decrypts an access token from the cookie.
+    // If no access token is present we use the CipherStash access key to do a token exchange and create a short-lived access token.
+    //
+    // This Web Crypto key is created from the provided CIPHERSTASH_KEY and is used to encrypt the access token sent to the user.
+    const key = await crypto.subtle.importKey(
+      "raw",
+      bufferFromHex(env.CIPHERSTASH_KEY),
+      { name: "AES-GCM" },
+      false,
+      ["encrypt", "decrypt"]
+    );
+
+    const cookies = `; ${request.headers.get("cookie") ?? ""}`;
+    const parts = cookies.split(`; ${TOKEN_COOKIE_NAME}=`);
+
+    if (parts.length === 2) {
+      const encrypted = parts.pop()?.split(";").shift();
+
+      // If there was an access token in the cookies - decrypt it and pass it through.
+      if (encrypted) {
+        const [iv, enc] = encrypted.split(".");
+
+        const decrypted = await crypto.subtle.decrypt(
+          {
+            name: "AES-GCM",
+            iv: bufferFromHex(iv),
+          },
+          key,
+          bufferFromHex(enc)
+        );
+
+        const decoder = new TextDecoder();
+        const token = decoder.decode(decrypted);
+
+        return await handler(request, token);
+      }
+    }
+
+    // Since there was no cached token - perform a token exchange with the CipherStash console and get a short lived token.
+    const response = await fetch(
+      "https://console.cipherstash.com/api/authorise",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          accessKey: env.CIPHERSTASH_CLIENT_SECRET,
+        }),
+        headers: {
+          "content-type": "application/json",
+        },
+      }
+    );
+
+    // If the token exchange failed it's probably because of a misconfiguration error.
+    if (response.status !== 200) {
+      throw new HandlerError(
+        "Failed to exchange auth token",
+        500,
+        await response.json<{}>().catch(() => undefined)
+      );
+    }
+
+    const { accessToken, expiry } = await response.json<{
+      accessToken: string;
+      expiry: number;
+    }>();
+
+    // Set the cookie to expire 10s before the actual access token expires.
+    // This ensures that the user will need to go back through the token exchange flow before the token has expired
+    // which saves them from running into any unauthorised errors.
+    const expires = new Date(expiry * 1000 - 10 * 1000);
+
+    const res = await handler(request, accessToken);
+
+    // Create a 16 byte IV for encrypting the
+    // This IV is stored along side the encrypted access token in the user cookie.
+    const iv = new Uint8Array(16);
+    crypto.getRandomValues(iv);
+
+    const encoder = new TextEncoder();
+
+    const encrypted = await crypto.subtle.encrypt(
+      {
+        name: "AES-GCM",
+        iv,
+      },
+      key,
+      encoder.encode(accessToken)
+    );
+
+    // Set the cookie on the response in the format <iv>.<ciphertext>
+    setCookie(
+      res,
+      `${bufferToHex(iv)}.${bufferToHex(new Uint8Array(encrypted))}`,
+      expires
+    );
+
+    return res;
+  };
+}
+
+/**
+ * Create a handler with a CipherStash client created from the current environment
+ *
+ * This handler also does a token exchange and authenticates with CipherStash using the CipherStash access key from the environment.
+ */
+export function withStash(
+  env: Env,
+  handler: (request: Request, stash: Stash) => Promise<Response>
+) {
+  return withAuth(env, async (request, token) => {
+    const stash = Stash.fromAnnotatedSchema(SCHEMA, {
+      host: `https://${SCHEMA.service.host}`,
+      key: env.CIPHERSTASH_KEY,
+      token,
+    });
+
+    return await handler(request, stash);
+  });
+}
