ca8078eba835f929ef8515d08fc084dcd61c9eb5
Lindsay Holmwood <lindsay@holmwood.id.au>
2022-11-14 11:47:03 +1100

    Clean the output of any control characters

--- scripts/create-access-key.sh
+++ scripts/create-access-key.sh
@@ -7,7 +7,7 @@ CREATE_KEY_OUTPUT_PATH="$(mktemp)"
 npx stash create-key --name cloudflare-worker > $CREATE_KEY_OUTPUT_PATH
 
 CLIENT_SECRET_PATH="$(mktemp)"
-grep CS_IDP_CLIENT_SECRET $CREATE_KEY_OUTPUT_PATH | cut -d '=' -f 2 > $CLIENT_SECRET_PATH
+grep -o 'CS_IDP_CLIENT_SECRET=[a-zA-Z0-9\.]*' $CREATE_KEY_OUTPUT_PATH | cut -d '=' -f 2 > $CLIENT_SECRET_PATH
 
 echo "Setting CIPHERSTASH_CLIENT_SECRET secret using Wrangler."
 cat $CLIENT_SECRET_PATH | wrangler secret put CIPHERSTASH_CLIENT_SECRET
