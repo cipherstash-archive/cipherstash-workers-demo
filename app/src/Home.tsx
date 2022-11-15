31e9091486f257bd4ae24f0c9079b00f956d530e
Bennett Hardwick <bennett@cipherstash.com>
2022-11-15 10:31:25 +1000

    Add success message, reset form

--- app/src/Home.tsx
+++ app/src/Home.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useMutation } from "react-query";
 import AdminLink from "./AdminLink";
 import { FormType, PatientForm } from "./PatientForm";
@@ -5,7 +6,8 @@ import { Spinner } from "./Spinner";
 import { apiUrl } from "./utils";
 
 export default function Home() {
-  const { mutate, isLoading } = useMutation(async (data: FormType) => {
+  const [formId, setFormId] = useState(0);
+  const { mutate, ...result } = useMutation(async (data: FormType) => {
     await fetch(apiUrl("/secure"), {
       method: "POST",
       body: JSON.stringify(data),
@@ -14,6 +16,8 @@ export default function Home() {
       },
       credentials: "include",
     });
+
+    setFormId((x) => x + 1);
   });
 
   return (
@@ -26,13 +30,29 @@ export default function Home() {
           reasons.
         </p>
         <div className="relative">
-          <PatientForm onSubmit={mutate} disabled={isLoading} />
-          {isLoading && (
+          <PatientForm
+            key={formId}
+            onSubmit={mutate}
+            disabled={result.status === "loading"}
+          />
+          {result.status === "loading" && (
             <div className="flex absolute bg-white/60 left-0 top-0 w-full h-full z-10 items-center justify-center">
               <div className="flex flex-col gap-6 p-8 bg-white drop-shadow border items-center">
                 <span className="text-xl">Submitting</span>
                 <Spinner />
               </div>
+            </div>
+          )}
+          {result.status === "success" && (
+            <div className="text-green-500 py-4 font-bold">
+              Successfully submitted form.
+            </div>
+          )}
+          {result.status === "error" && (
+            <div className="text-red-500 py-4">
+              <span>Failed to submit patient information.</span>
+              <br />
+              Error: {String(result.error)}
             </div>
           )}
         </div>
